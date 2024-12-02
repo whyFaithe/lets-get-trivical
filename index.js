47a17c9d392fa9d73761afe074720953147af917
ads490707 <133542462+ads490707@users.noreply.github.com>
2024-12-01 20:55:03 -0500

    update react

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client"; // Use "react-dom/client" for createRoot
+import App from "./App";
 import { ScreenProvider } from "./ScreenContext";
 
-ReactDOM.render(
-    <ScreenProvider>
-        <App />
-    </ScreenProvider>
-, document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create root
+
+root.render(
+  <ScreenProvider>
+    <App />
+  </ScreenProvider>
+);
